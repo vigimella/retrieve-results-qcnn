7b179d38011a35b21e5d4548a9df1e5b6b4f2fa4
vigimella <g.ciaramella98@gmail.com>
2023-10-31 10:24:33 +0100

    Updated path

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import pandas as pd
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
-def generate_csv_file(outputfile, file_list, loss_list, acc_list, pre_list, rec_list, roc_list):
+def generate_csv_file(outputfile, file_list, loss_list, acc_list, pre_list, rec_list, roc_list,
+                      epochs_list, lr_list, bs_list, ts_list):
     data = {
         'File': file_list,
+        'Epochs': epochs_list,
+        'Learning Rate': lr_list,
+        'Batch Size': bs_list,
+        'Threshold': ts_list,
         'Loss': loss_list,
         'Accuracy': acc_list,
         'Precision': pre_list,
@@ -23,7 +28,7 @@ def generate_csv_file(outputfile, file_list, loss_list, acc_list, pre_list, rec_
 
 
 def retrieve_results(zip_file_name, main_directory, storage_directory, loss_list, acc_list, pre_list, rec_list,
-                     roc_list):
+                     roc_list, epochs_list, lr_list, bs_list, ts_list):
     zip_file_path = os.path.join(main_directory, zip_file_name)
 
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
@@ -46,14 +51,44 @@ def retrieve_results(zip_file_name, main_directory, storage_directory, loss_list
                         pre_list.append(float(line[2]))
                         rec_list.append(float(line[3]))
                         roc_list.append(float(line[4]))
+                    elif 'Epochs:' in line:
+
+                        line = line.replace('Epochs:', '').replace(' \n', '')
+                        epochs_list.append(line)
+
+                    elif 'Batch Size:' in line:
+
+                        line = line.replace('Batch Size:', '').replace(' \n', '')
+                        bs_list.append(line)
+
+                    elif 'Learning Rate:' in line:
+
+                        line = line.replace('Learning Rate:', '').replace(' \n', '')
+                        lr_list.append(line)
+
+                    elif 'Threshold:' in line:
+                        line = line.replace('Threshold:', '').replace(' \n', '')
+                        ts_list.append(line)
 
     shutil.rmtree(storage_directory)
 
 
 if __name__ == '__main__':
 
+    ans_q = input('Are the zipper files stored in this folder? (Y/N): ')
+
+    if ans_q.lower() == 'y':
+        folder_to_store_zip_files = os.path.join(APP_ROOT, 'zip')
+    else:
+        new_path = input('Insert new path: ')
+        folder_to_store_zip_files = os.path.join(APP_ROOT, new_path)
+
     folder_to_store_unzipped_files = os.path.join(APP_ROOT, 'unzipped')
-    folder_to_store_zip_files = os.path.join(APP_ROOT, 'zip')
+
+    epochs = list()
+    learning_rate = list()
+    batch_size = list()
+    threshold = list()
 
     test_files_name = list()
     test_loss = list()
@@ -75,7 +110,10 @@ if __name__ == '__main__':
             print(f'Unzipping: {file}')
             retrieve_results(zip_file_name=file, main_directory=folder_to_store_zip_files,
                              storage_directory=folder_to_store_unzipped_files, loss_list=test_loss, acc_list=test_acc,
-                             pre_list=test_pre, rec_list=test_rec, roc_list=test_roc)
+                             pre_list=test_pre, rec_list=test_rec, roc_list=test_roc, epochs_list=epochs,
+                             bs_list=batch_size, lr_list=learning_rate, ts_list=threshold)
+
     out = os.path.join(APP_ROOT, 'out.csv')
     generate_csv_file(outputfile=out, file_list=test_files_name, loss_list=test_loss, acc_list=test_acc,
-                      pre_list=test_pre, rec_list=test_rec, roc_list=test_roc)
+                      pre_list=test_pre, rec_list=test_rec, roc_list=test_roc, epochs_list=epochs, lr_list=learning_rate,
+                      ts_list=threshold, bs_list=batch_size)
